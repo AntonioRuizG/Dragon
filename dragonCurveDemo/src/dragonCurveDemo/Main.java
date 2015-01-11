d04615c1dff3153fb70fb3da7256e4679c40a5b2
Antonio Ruiz Gutierrez <aruizgutierrez2014@gmail.com>
2015-01-11 15:21:12 +0100

    Window smaller, nice coloration, R starting curve

--- dragonCurveDemo/src/dragonCurveDemo/Main.java
+++ dragonCurveDemo/src/dragonCurveDemo/Main.java
@@ -5,6 +5,7 @@ public class Main
 {
     private static HashMap<String, String> rules = new HashMap<String, String>();
     private static HashMap<String, String> rules2 = new HashMap<String, String>();
+    private static Window window;
     
     public static void main(String[] args)
     {
@@ -17,9 +18,9 @@ public class Main
         rules2.put("l", "L");
         rules2.put("R", "R");
         rules2.put("L", "L");
-        Window w = new Window(1200,1000);
-        String dragon = proce("L",14);
-        w.drawDragon(dragon,800,750,3);
+        window = new Window(640,480);
+        String dragon = proce("R",14);
+        drawDragon(dragon,230,350,2);
     }
     
     private static String proce(String input, int level){
@@ -43,5 +44,65 @@ public class Main
         }
         return output.toString();
     }
-    
+	
+    public static void drawDragon(String input, double x, double y, double size){
+        char[] intputChar = input.toCharArray();
+        double[] p1 = new double[]{x,y};
+        double[] p2 = new double[]{x+size,y};
+        int direccion=0;
+        for(int i=0;i<intputChar.length;i++){
+            
+            if(intputChar[i]=='R'){
+                direccion =(direccion+1)%4;
+                
+            }else{
+                direccion =(direccion+3)%4;
+            }
+            switch(direccion){
+                case 0:
+                    p1[0]=p2[0];
+                    p1[1]=p2[1];
+                    
+                    p2[0]=p1[0]+size;
+                    p2[1]=p1[1];
+                break;
+                case 1:
+                    p1[0]=p2[0];
+                    p1[1]=p2[1];
+                    
+                    p2[0]=p1[0];
+                    p2[1]=p1[1]+size;
+                break;
+                case 2:
+                    p1[0]=p2[0];
+                    p1[1]=p2[1];
+                    
+                    p2[0]=p1[0]-size;
+                    p2[1]=p1[1];
+                break;
+                case 3:
+                    p1[0]=p2[0];
+                    p1[1]=p2[1];
+                    
+                    p2[0]=p1[0];
+                    p2[1]=p1[1]-size;
+                break;
+            }
+            if(i>(intputChar.length/2))window.changeBrushColor(1,0,0);
+            else if(i>(intputChar.length/4))window.changeBrushColor(0,0,0);
+            else if(i>(intputChar.length/8))window.changeBrushColor(1,0,0);
+            else if(i>(intputChar.length/16))window.changeBrushColor(0,0,0);
+            else if(i>(intputChar.length/32))window.changeBrushColor(1,0,0);
+            else if(i>(intputChar.length/64))window.changeBrushColor(0,0,0);
+            else if(i>(intputChar.length/128))window.changeBrushColor(1,0,0);
+            
+            window.drawLine(p1[0], p1[1], p2[0], p2[1]);
+            
+            try{
+                Thread.sleep(1);
+            }catch(Exception e){
+                
+            }
+        }
+    }
 }
--- dragonCurveDemo/src/dragonCurveDemo/Window.java
+++ dragonCurveDemo/src/dragonCurveDemo/Window.java
@@ -1,12 +1,13 @@
 ﻿package dragonCurveDemo;
 import java.awt.Graphics2D;
 import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 import java.awt.Image;
 import java.awt.Graphics;
-import java.awt.Shape;
-import java.awt.Dimension;
+import java.awt.RenderingHints;
 
 /**
  * A window to draw simple graphics.
@@ -19,8 +20,8 @@ public class Window extends JFrame
 	private static final long serialVersionUID = 6484935181546031766L;
 	private final Image imagen;
     private Panel panel;
-    private Color colorDeFondo;
     private Color colorBrocha=Color.red;
+	private Graphics2D gr;
 
     /**
      * 
@@ -35,8 +36,10 @@ public class Window extends JFrame
         setSize(width, height);
         panel=new Panel();
         setContentPane(panel);
-        this.colorDeFondo=Color.white;
         imagen = createImage(width, height);
+        gr = (Graphics2D)imagen.getGraphics();
+    	gr.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+    	        RenderingHints.VALUE_ANTIALIAS_ON);
         
     }
     
@@ -46,89 +49,13 @@ public class Window extends JFrame
     }
     
     public void drawLine(double x1, double y1, double x2, double y2){
-        Graphics2D g = (Graphics2D)imagen.getGraphics();
-        g.setColor(colorBrocha);
-        g.drawLine((int)x1, (int)y1, (int)x2, (int)y2);
+        gr.drawLine((int)x1, (int)y1, (int)x2, (int)y2);
         redraw();
-    }
-    
-    public void dibujar(Shape e)
-    {
-        Graphics2D g = (Graphics2D)imagen.getGraphics();
-        g.setColor(Color.red);
-        g.fill(e);
-    } 
-    
-    public Graphics2D getGraphics2D()
-    {
-        return (Graphics2D)imagen.getGraphics();
-    }
-    
-    public void borrar()
-    {
-        Graphics2D g = (Graphics2D)imagen.getGraphics();
-        g.setColor(colorDeFondo);
-        Dimension d = panel.getSize();
-        g.fillRect(0,0,d.width,d.height);
-    }
-    
-    public void drawDragon(String input, double x, double y, double size){
-        char[] intputChar = input.toCharArray();
-        double[] p1 = new double[]{x,y};
-        double[] p2 = new double[]{x+size,y};
-        int direccion=0;
-        for(int i=0;i<intputChar.length;i++){
-            
-            if(intputChar[i]=='R'){
-                direccion =(direccion+1)%4;
-                
-            }else{
-                direccion =(direccion+3)%4;
-            }
-            switch(direccion){
-                case 0:
-                    p1[0]=p2[0];
-                    p1[1]=p2[1];
-                    
-                    p2[0]=p1[0]+size;
-                    p2[1]=p1[1];
-                break;
-                case 1:
-                    p1[0]=p2[0];
-                    p1[1]=p2[1];
-                    
-                    p2[0]=p1[0];
-                    p2[1]=p1[1]+size;
-                break;
-                case 2:
-                    p1[0]=p2[0];
-                    p1[1]=p2[1];
-                    
-                    p2[0]=p1[0]-size;
-                    p2[1]=p1[1];
-                break;
-                case 3:
-                    p1[0]=p2[0];
-                    p1[1]=p2[1];
-                    
-                    p2[0]=p1[0];
-                    p2[1]=p1[1]-size;
-                break;
-            }
-            if(i>(intputChar.length/2))changeBrushColor(0,0,0);
-            
-            drawLine(p1[0], p1[1], p2[0], p2[1]);
-            
-            try{
-                Thread.sleep(1);
-            }catch(Exception e){
-                
-            }
-        }
     }
     
     public void changeBrushColor(double r, double g, double b){
         colorBrocha=new Color((float)r, (float)g, (float)b);
+        gr.setColor(colorBrocha);
     }
     
     private class Panel extends JPanel
